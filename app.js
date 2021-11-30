b60d4316102869d8c757996fb4997d66777f3271
Luiz Falcao <luizotavio-falcao@hotmail.com>
2021-11-29 21:09:53 -0300

    Add GET and POST method for new messages

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const newMsgRouter = require('./routes/new-message');
 
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,15 +20,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/new', newMsgRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Dietrich',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Ophelia',
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Mini Message Board', messages });
+});
+
+router.get('/new', function (req, res, next) {
+  res.render('form', { title: 'New message' });
+});
+
+router.post('/new', function (req, res, next) {
+  const { user, message: text } = req.body;
+  messages.push({ text, user, added: new Date() });
+  // Fix infinite loading
 });
 
 module.exports = router;
